047e2d9149fec0e24b38c6c1e3e31c3d61ead52f
DougBrownWin <DougBrownWin@DESKTOP-9LQ38S7>
2018-01-29 20:55:00 -0700

    just adding test

---
+++ src/test.py
@@ -0,0 +1,15 @@
+'''
+Created on Jan 29, 2018
+
+@author: DougBrownWin
+'''
+
+if __name__ == '__main__':
+    pass
+
+    import numpy as np
+    
+    
+    s = 5
+    print(np.identity(16)[s])
+    print(np.identity(16)[s:s+1])
